cc9b6aa0bc2f49da7c93b6df273d0f6a7fb39ce9
Anik Das <anikdas@Aniks-MacBook-Pro.local>
2020-03-29 05:05:02 +0300

    adding help-page

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -5,7 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name='home'),
     path('count/', views.countwords, name='count'),
-    path('about/', views.aboutpage, name='about')
+    path('about/', views.aboutpage, name='about'),
+    path('help/', views.helppage, name='help')
+
+
 
 
 ]
--- wordcount/views.py
+++ wordcount/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request,'home.html')
+
+def helppage(request):
+    return render(request,'help.html')
+
 def aboutpage(request):
     return render(request, 'about.html')
 
